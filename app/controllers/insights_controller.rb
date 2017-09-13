5e27a5fa3c97c5551e743d3060983e403d2ecc2c
doverh <doverheld@gmail.com>
2017-09-13 14:32:30 -0400

    Edit and delete functionality

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,6 +1,8 @@
 class InsightsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 
 	def index
+		@insights = Insight.all.order("created_at DESC")
 	end
 
 	def new
@@ -9,11 +11,39 @@ class InsightsController < ApplicationController
 		
 	def create
 		@insight = Insight.new(insight_params)
+
+		if @insight.save
+			redirect_to @insight, notice: "Sucessfully created new Insight"
+		else
+			render "new"
+		end
+
 	end
+
+	def edit
+	end
+
+	def update
+		if @insight.update(insight_params)
+			redirect_to @insight, notice:"Insight sucessfully updated!"
+		else
+			render 'edit'	
+		end
+	end
+
+	def destroy
+		@insight.destroy
+		redirect_to root_path
+	end
+
 
 	private 
 
 	def insight_params
 		params.require(:insight).permit(:title, :description)
+	end
+
+	def find_pin
+		@insight = Insight.find(params[:id])
 	end
 end
